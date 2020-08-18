0c98e2701f53487c7171a1c88beaae31d402b865
lilyhlou <lilylou16@gmail.com>
2020-08-18 15:40:23 -0400

    centered links in navbar and added gitignore

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,26 @@
 import PropTypes from "prop-types"
 import React from "react"
-
-
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 const Header = ({ siteTitle }) => (
-  <nav className="navbar navbar-expand-md navbar-light bg-light sticky-top navbar-transparent">
-  <a className="navbar-brand" href="#">{siteTitle}</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-      <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul className="navbar-nav">
-          <li className="nav-item active">
-              <a className="nav-link" href="#home">Home</a>
-          </li>
-          <li className="nav-item">
-              <a className="nav-link" href="#code">Code</a>
-          </li>
-          <li className="nav-item">
-              <a className="nav-link" href="#writing">Writing</a>
-          </li>
-          <li className="nav-item">
-              <a className="nav-link" href="#about">About</a>
-          </li>
-      </ul>
-  </div>
-</nav>
+    <Navbar collapseOnSelect expand="lg" className="color-nav" variant="light">
+    <Navbar.Brand>{siteTitle}</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto" activeKey="/home">
+      <Nav.Link href="/#main">Home</Nav.Link>
+      <Nav.Link href="/#code">Code</Nav.Link>
+      <Nav.Link href="/#writing">Writing</Nav.Link>
+      <Nav.Link href="/#about">About</Nav.Link>
+
+    </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+
 )
+
+
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
