dffcf02124980955906001ee9d39a75d30b71fcc
lilyhlou <lilylou16@gmail.com>
2020-08-22 16:43:37 -0400

    Laid out projects images

--- src/components/projects-page.js
+++ src/components/projects-page.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import {Card, CardTitle, CardText, CardSubtitle, CardBody} from 'react-bootstrap'
+import {Card, CardTitle, CardText, CardSubtitle, CardBody, CardImg, CardImgOverlay} from 'react-bootstrap'
 import Img from "gatsby-image"
 
-const Page = ({ title, subtitle, path, date, body, image}) => {
-
+const Page = ({ title, subtitle, path, date, body, tech, image}) => {
     return(
-        <div>
-        <p> {title} </p>
-        <p> {subtitle} </p>
-        <p> {path} </p>
-        <p> {date} </p>
-        <p> {body} </p>
-        <Img fluid={image} />
-
-        </div>
+        <Card>
+            <Card.Img fluid={image} alt="Card image" />
+            <Card.ImgOverlay>
+                <Card.Title> {title} </Card.Title>
+                <Card.Subtitle> {subtitle} </Card.Subtitle>
+                <Card.Subtitle> {path} </Card.Subtitle>
+                <Card.Subtitle> {date} </Card.Subtitle>
+                <Card.Subtitle> {tech} </Card.Subtitle>
+                <Card.Body> {body} </Card.Body>
+                <Img fluid={image} />
+            </Card.ImgOverlay>
+        </Card>
 
     )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,48 +10,28 @@ import {Card, CardGroup} from 'react-bootstrap'
 const Projects = () => (
   <div className="flex" id="projects">
     <h3>Projects and Code</h3>
-    <CardGroup>
-    <Card className="bg-dark text-white">
-    <Card.Title>Card title</Card.Title>
-    <Card.Text>
-      This is a wider card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-    </Card.Text>
-    <Card.Text>Last updated 3 mins ago</Card.Text>
-</Card>
-<Card className="bg-dark text-white">
-    <Card.Title>Card title</Card.Title>
-    <Card.Text>
-      This is a wider card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-    </Card.Text>
-    <Card.Text>Last updated 3 mins ago</Card.Text>
-</Card>
 
-</CardGroup>
     <StaticQuery 
       query={projectsQuery}
       render={data => {
         return (
-          <div>
+          <CardGroup>
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div>
             <Page 
               title={node.frontmatter.title}
               subtitle={node.frontmatter.subtitle}
               path={node.frontmatter.path} 
               date={node.frontmatter.date} 
-              body={node.frontmatter.tech}
+              body={node.excerpt}
+              tech={node.frontmatter.tech}
               image={node.frontmatter.image.childImageSharp.fluid}
+              
                           />
-          <h1>PROJECT</h1>
-
-
-          </div>
         ))}
-        </div>
+        </CardGroup>
       )
       }}/>
+
   </div>
 )
 
@@ -60,6 +40,7 @@ const projectsQuery = graphql`
   allMarkdownRemark {
     edges {
       node {
+        id
         frontmatter {
           title
           path
@@ -70,17 +51,12 @@ const projectsQuery = graphql`
           image {
             childImageSharp {
               fluid {
-
-                ...GatsbyImageSharpFluid
-                ...GatsbyImageSharpFluidLimitPresentationSize
-
-
-
+                ...GatsbyImageSharpFluid 
               }
             }
           }
-
         }
+        excerpt
       }
     }
   }
